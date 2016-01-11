efee6f37725ed5714ea1447356d50168fd2ac8fd
c1ko <ciko@shackspace.de>
2016-01-11 20:04:15 +0100

    initial

---
+++ main.js
@@ -0,0 +1,52 @@
+function updateSack(){
+	sackRequest = new XMLHttpRequest();
+	sackRequest.open("GET", "https://meinsack.click/v1/70372/Daimlerstra%C3%9Fe/ical/", true);
+
+	sackRequest.onreadystatechange=function(){
+		if(sackRequest.readyState==4 && sackRequest.status==200){
+			lines = sackRequest.responseText.split("\n")
+			for(var i = 0; i<lines.length; i++){
+				line = lines[i]
+				if(line.substring(0,7) == "DTSTART"){ //Get the correct lines from the calender
+					ds = line.substring(line.length-9, line.length-1)
+					year = ds.substring(0, 4)
+					month = ds.substring(4, 6)
+					day = ds.substring(6, 8)
+
+					thatdate = day + "/" + month + "/" + year
+					md = new Date(year, month, day, 0, 0, 0, 0).getTime();	
+					cd = new Date().getTime()	
+					if(md-cd > 0){ //Chose the first one that is not in the past
+						if(md-cd < 1000*60*60*24){
+							document.getElementById("sack").innerHTML = "MORGEN";
+						}
+						else{
+							document.getElementById("sack").innerHTML = thatdate
+						}
+
+						break
+					}
+				}
+			}
+		}
+	}
+	sackRequest.send();
+}
+
+function updateDate(){
+	d = new Date();
+	document.getElementById("date").innerHTML = d.getDate()+1 + "/" + d.getMonth()+1 + "/" + d.getFullYear();
+}
+
+document.onreadystatechange = function() {
+	var state = document.readyState;
+	if(state == 'complete') {
+		updateSack();
+		updateDate();
+		
+		setInterval(function(){
+			updateSack();
+			updateDate();
+		}, 1000*60*30); //Update every 30 minutes
+	}
+}
