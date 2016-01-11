686df14b5c8192438cde547f0f2233a6016ba371
c1ko <ciko@shackspace.de>
2016-01-11 20:13:51 +0100

    development

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function updateDate(){
-	document.getElementById("date").innerHTML = "d.getDate()+1 + "/" + d.getMonth()+1 + "/" + d.getFullYear();"
+	document.getElementById("date").innerHTML = d.getDate()+1 + "/" + d.getMonth()+1 + "/" + d.getFullYear();
 }
 
 //updateSack();
